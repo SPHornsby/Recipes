fc534c96839986fc088324d3579a9b8145dc317d
Sean Hornsby <sean.hornsby@gmail.com>
2019-10-15 07:46:48 -0700

    Launch screen

--- Recipes/ViewController.swift
+++ Recipes/ViewController.swift
@@ -18,12 +18,24 @@ class ViewController: UITableViewController {
         tableView.backgroundView = UIImageView(image: UIImage(named: "breads"))
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        navigationController?.navigationBar.alpha = 0.5
+    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return array.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customcell") as! UITableViewCell
+        if (indexPath.item % 2 == 0) {
+            cell.backgroundColor = UIColor.clear
+        } else {
+            cell.backgroundColor = UIColor.white.withAlphaComponent(0.2)
+            cell.textLabel?.backgroundColor = UIColor.white.withAlphaComponent(0.0)
+        }
+        cell.textLabel?.textColor = UIColor.white
         cell.textLabel?.text = array[indexPath.item]
         return cell
     }
