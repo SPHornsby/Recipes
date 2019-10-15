875aac58029080eecad7905245392c6a7e1dcdc8
Sean Hornsby <sean.hornsby@gmail.com>
2019-10-15 07:27:09 -0700

    Scenes flowing

--- Recipes/ViewController.swift
+++ Recipes/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    let array = ["first", "second", "third", "fourth"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.rowHeight = 70
+        tableView.backgroundView = UIImageView(image: UIImage(named: "breads"))
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customcell") as! UITableViewCell
+        cell.textLabel?.text = array[indexPath.item]
+        return cell
+    }
 }
 
